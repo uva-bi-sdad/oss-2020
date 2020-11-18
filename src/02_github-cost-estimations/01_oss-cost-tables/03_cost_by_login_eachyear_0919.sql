603998275b057b33564bba6e027b53c10a1444d7
brandonleekramer <brandonleekramer@gmail.com>
2020-11-18 17:41:04 -0500

    cost calcs for us in 2019

--- src/02_github-cost-estimations/01_oss-cost-tables/03_cost_by_login_eachyear_0919.sql
+++ src/02_github-cost-estimations/01_oss-cost-tables/03_cost_by_login_eachyear_0919.sql
@@ -1,17 +1,16 @@
 
--- second, we want all the commits, additions, deletions, sum and net by year (2019)
--- this is not by login/year - just by year
+-- second, we want all the commits, additions, deletions, sum and net by login within repo for each year (2008-2019)
 
-CREATE MATERIALIZED VIEW gh.cost_by_year_0919 AS (
+CREATE MATERIALIZED VIEW gh.cost_by_login_eachyear_0919 AS (
 WITH commits_annual AS (
 SELECT slug, login, additions, deletions, EXTRACT(YEAR FROM committed_date)::int AS year
 FROM gh.commits_raw
 )
 
-SELECT slug, year, COUNT(*) AS commits, SUM(additions) AS additions, SUM(deletions) AS deletions,
+SELECT slug, login, year, COUNT(*) AS commits, SUM(additions) AS additions, SUM(deletions) AS deletions,
 					SUM(additions + deletions) AS sum_adds_dels, SUM(additions - deletions) AS net_adds_dels
 FROM commits_annual
 WHERE year > 2008 AND year < 2020
-GROUP BY slug, year );
+GROUP BY slug, login, year );
 
 GRANT ALL PRIVILEGES ON TABLE gh.cost_by_year_0919 TO ncses_oss;
