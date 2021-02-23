033142e9d46ecb865bc9ec96f56a98d19427f5c1
José Bayoán Santiago Calderón <jbs3hp@virginia.edu>
2021-02-23 11:36:13 -0500

    ACR
    Signed-off-by: José Bayoán Santiago Calderón <jbs3hp@virginia.edu>

--- src/01_github-wrangling/06_commits_nmrc_jbsc.sql
+++ src/01_github-wrangling/06_commits_nmrc_jbsc.sql
@@ -1,4 +1,5 @@
 create materialized view gh.commits_freq as (
+    -- Find how many times a commit show up
     with a as (
         SELECT committed_date,
             login,
@@ -10,7 +11,7 @@ create materialized view gh.commits_freq as (
             login,
             additions,
             deletions
-    )
+    ) -- Find all commits which appear in multiple repos
     select *
     from a
     where count > 1
@@ -21,19 +22,20 @@ create materialized view gh.commits_freq as (
         deletions
 );
 create materialized view gh.commits_freq_0 as (
- SELECT a.slug,
-    a.committed_date,
-    a.login,
-    a.additions,
-    a.deletions
-   FROM gh.commits_dd a
-   JOIN gh.commits_freq b
-   ON a.committed_date = b.committed_date
-   AND a.login = b.login
-   AND a.additions = b.additions
-   AND a.deletions = b.deletions
+    -- Find the slugs of each commit that appears in multiple repos
+    SELECT a.slug,
+        a.committed_date,
+        a.login,
+        a.additions,
+        a.deletions
+    FROM gh.commits_dd a
+        JOIN gh.commits_freq b ON a.committed_date = b.committed_date
+        AND a.login = b.login
+        AND a.additions = b.additions
+        AND a.deletions = b.deletions
 );
 create materialized view gh.commits_freq_1 as (
+    -- Compute the earliest and most recent "shared" commit for each repo
     with a as (
         SELECT slug,
             max(committed_date),
@@ -41,6 +43,9 @@ create materialized view gh.commits_freq_1 as (
         FROM gh.commits_freq_0
         group by slug
     ),
+    -- Compute the duration from earliest to most recent "shared" commit
+    -- in each repo and keep only the slug of the repo with the longest
+    -- chain
     b as (
         select distinct on (slug) slug,
             max - min as duration
@@ -48,12 +53,15 @@ create materialized view gh.commits_freq_1 as (
         order by slug,
             duration desc
     )
+    -- For each commit, assign the repo in which it shows up having the
+    -- longest chain
     select distinct a.*
     from gh.commits_freq_0 a
-    join b
-    on a.slug = b.slug
+        join b on a.slug = b.slug
 );
 create materialized view gh.commits_dd_nmrc_jbsc as (
+    -- Have original records and the "refined" records
+    -- give preference to refined ones over the orginal ones
     with a as (
         select *,
             false to_keep
@@ -63,6 +71,7 @@ create materialized view gh.commits_dd_nmrc_jbsc as (
             true to_keep
         from gh.commits_freq_1
     ),
+    -- Remove original records for which there is a refined one
     b as (
         select distinct on (committed_date, login, additions, deletions) *
         from a
@@ -72,6 +81,7 @@ create materialized view gh.commits_dd_nmrc_jbsc as (
             deletions,
             to_keep desc
     )
+    -- Final clean up which has every commit just ones based on the refinement
     select distinct slug,
         committed_date,
         login,
