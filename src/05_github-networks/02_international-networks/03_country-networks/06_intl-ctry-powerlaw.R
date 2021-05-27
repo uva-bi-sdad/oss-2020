a760d51390db383e181a032d677fd9f4bd75b394
brandonleekramer <brandonleekramer@gmail.com>
2021-05-27 13:03:02 -0400

    adding note to scraping code

--- src/05_github-networks/02_international-networks/03_country-networks/06_intl-ctry-powerlaw.R
+++ src/05_github-networks/02_international-networks/03_country-networks/06_intl-ctry-powerlaw.R
@@ -110,19 +110,45 @@ test_if_power_law <- function(analysis_year){
   m_poi$setXmin(est_poi)
 
   # save all the results in the same place
-  alpha <- est$pars
-  alpha_sd <- sd(bs_pl$bootstraps$pars)
+  alpha <- round(est$pars, 3)
+  alpha_sd <- round(sd(bs_pl$bootstraps$pars), 3)
   xmin <- est$xmin
-  xmin_sd <- sd(bs_pl$bootstraps$xmin)
-  gof <- bs_pl$gof
-  pvalue <- bs_pl$p
-  power_law_data <- as.data.frame(cbind(analysis_year, alpha, alpha_sd, xmin, xmin_sd, pvalue, gof))
+  xmin_sd <- round(sd(bs_pl$bootstraps$xmin), 3)
+  gof <- round(bs_pl$gof, 3)
+  pvalue <- round(bs_pl$p, 3)
 
   # interpretating the p-value to reject or accept the power law test
   # Calculate the goodness-of-fit between the data and the power law using the
   # method described in section 4. If the resulting p-value is greater than 0.1, the
   # power law is a plausible hypothesis for the data, otherwise it is rejected.
   # found on page 663 of the clauset et al. 2009 paper
+
+  # if the p-value is greater than 0.1, clauset et al recommend testing
+  # alternative hypotheses via a likelihood ratio test, as described in section 5.
+  # For _each_ alternative, if the calculated likelihood ratio is significantly
+  # different from zero, then its sign indicates whether or not the
+  # alternative is favored over the power-law model. To do this, I used:
+  # section 1.3 of https://cran.r-project.org/web/packages/poweRlaw/vignettes/b_powerlaw_examples.pdf
+
+  m_ln$setXmin(m_pl$getXmin())
+  est_comp = estimate_pars(m_ln)
+  m_ln$setPars(est_comp)
+  comp = compare_distributions(m_pl, m_ln)
+  ln_comp = round(comp$p_two_sided, 3)
+
+  m_exp$setXmin(m_pl$getXmin())
+  est_comp = estimate_pars(m_exp)
+  m_exp$setPars(est_comp)
+  comp = compare_distributions(m_pl, m_exp)
+  exp_comp = round(comp$p_two_sided, 3)
+
+  m_poi$setXmin(m_pl$getXmin())
+  est_comp = estimate_pars(m_poi)
+  m_poi$setPars(est_comp)
+  comp = compare_distributions(m_pl, m_poi)
+  poi_comp = round(comp$p_two_sided, 3)
+
+  power_law_data <- as.data.frame(cbind(analysis_year, alpha, alpha_sd, xmin, xmin_sd, pvalue, gof, ln_comp, exp_comp, poi_comp))
 
   setwd("~/git/oss-2020/data/network-analysis/intl-ctry-nets-cum/wisos-lchn/")
   saveRDS(power_law_data, str_c("power_law_data_",analysis_year,".rds"))
@@ -131,8 +157,7 @@ test_if_power_law <- function(analysis_year){
 
 ##################################################################################### for loop of all years
 
-for (year in c("0809", "0810", "0811", "0812", "0813", "0814", "0815", "0816", "0817", "0818", "0819")) {
-#for (year in c("08", "0809", "0810", "0811", "0812", "0813", "0814", "0815", "0816", "0817", "0818", "0819")) {
+for (year in c("08", "0809", "0810", "0811", "0812", "0813", "0814", "0815", "0816", "0817", "0818", "0819")) {
   test_if_power_law(year)
 }
 
