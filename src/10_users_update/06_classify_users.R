19cd3bf2bd3a59a5b7cbb0f3de6f57f78ce22c2b
Brandon Kramer <brandonleekramer@gmail.com>
2021-10-13 15:03:21 -0400

    adding summaries

--- src/10_users_update/06_classify_users.R
+++ src/10_users_update/06_classify_users.R
@@ -1,5 +1,6 @@
 
 devtools::install_github("brandonleekramer/tidyorgs")
+devtools::install_github("brandonleekramer/diverstidy", force = TRUE)
 
 rm(list = ls())
 library(tidyverse)
@@ -13,11 +14,14 @@ conn <- dbConnect(drv = PostgreSQL(),
                   port = 5432,
                   user = Sys.getenv("db_userid"),
                   password = Sys.getenv("db_pwd"))
-github_users <- dbGetQuery(conn, "SELECT login, company, location, email FROM gh.ctrs_clean_0821")
+github_users <- dbGetQuery(conn, "SELECT login, company, location, email
+                           FROM gh.ctrs_clean_0821")
 dbDisconnect(conn)
 
+github_users <- readRDS("data/github_ctrs_clean_0821.rds")
+
 start_time = Sys.time()
-classified_by_orgs <- github_users %>%
+classified_users <- github_users %>%
   detect_orgs(login, company, organization, academic, email,
               country = TRUE, parent_org = TRUE, org_type = TRUE) %>%
   rename(ac_country = country) %>%
@@ -26,30 +30,79 @@ end_time = Sys.time()
 time_to_classify = end_time - start_time; time_to_classify
 # took just over an hour with 3.2m users
 
-saveRDS(classified_users, "~/git/oss-2020/data/sectored_100821.rds")
+saveRDS(classified_users, "data/github_sectored_101321.rds")
+classified_users <- readRDS("data/github_sectored_101321.rds")
 
-with_some_info <- classified_by_orgs %>%
+with_some_info <- classified_users %>%
   filter(!is.na(location) | !is.na(company) | !is.na(email))
 
-# check for NAs
-with_country_data <- classified_by_orgs %>%
+with_valid <- classified_users %>%
+  drop_na(location)
+nrow(with_valid) / nrow(classified_users)
+
+with_loc_email <- classified_users %>%
+  filter((!is.na(location) | !is.na(email)))
+
+with_comp_email <- classified_users %>%
+  filter((!is.na(company) | !is.na(email)))
+
+with_country_data <- classified_users %>%
   filter(!is.na(country) | !is.na(ac_country)) %>%
-  mutate(new_country = if_else(is.na(country), ac_country, country))
+  mutate(country = if_else(is.na(country), ac_country, country))
+
+country_counts <- with_country_data %>%
+  unnest_legacy(country = base::strsplit(country, "\\|")) %>%
+  filter(country != "NA") %>%
+  group_by(country) %>%
+  count() %>%
+  arrange(-n)
+
+with_academic_data <- classified_users %>%
+  filter(academic == 1) %>%
+  select(login, company, organization, email, academic, everything())
+
+academic_counts <- with_academic_data %>%
+  unnest_legacy(organization = base::strsplit(organization, "\\|")) %>%
+  group_by(organization) %>%
+  count() %>%
+  arrange(-n)
+# university of wisconsion "madison"
+# university of University of Michigan-Ann Arbor
+# École Polytechnique de Montréal Université De Montréal
+# University of Maryland-College Park-Baltimore County
+
+chk <- with_academic_data %>%
+  filter(grepl("\\|", organization))
 
 
-with_country_data %>%
-  dplyr::mutate(sector = replace_na(org_type, 0))
+
+checking_academia <- with_academic_data %>%
+  left_join(academic_counts, by = "organization") %>%
+  filter(organization != "Misc. Academic" & n < 232) %>%
+  arrange(-n, organization, company) %>%
+  select(login, company, organization, email, n, location)
+
+nrow(with_country_data) / nrow(with_some_info) # 78%
+nrow(with_country_data) / nrow(with_loc_email) # 82%
+nrow(with_country_data) / nrow(with_loc_email)
+
+# issues with country info ###############
+# still introducing NAs at some point
 
 
 
+checking_misc <- with_academic_data %>%
+  left_join(academic_counts, by = "organization") %>%
+  filter(organization == "Misc. Academic") %>%
+  arrange(-n, organization, company) %>%
+  select(login, company, organization, email, n, location) %>%
+  mutate(company = tolower(company)) %>%
+  count(company) %>%
+  arrange(-n)
 
 
 
-
-
-
-
-
+exempt_orgs <- read_rds("data/ExemptOrganizations.rds")
 
 
 
